6114062c656ff13c7d264eb1a21b4b86dd199ddd
Pandu Supriyono <mail@supriyono.nl>
2023-02-19 20:55:30 +0100

    setup pino logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,30 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { validateConfig } from './config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { LoggerModule } from 'nestjs-pino';
+import { EnvironmentVariables, validateConfig, Environment } from './config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       validate: validateConfig,
+    }),
+    LoggerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (
+        configService: ConfigService<EnvironmentVariables, true>,
+      ) => ({
+        pinoHttp: {
+          level:
+            configService.get('NODE_ENV') === Environment.Production
+              ? 'info'
+              : 'debug',
+          transport:
+            configService.get('NODE_ENV') !== Environment.Production
+              ? { target: 'pino-pretty' }
+              : undefined,
+        },
+      }),
     }),
   ],
   controllers: [],
