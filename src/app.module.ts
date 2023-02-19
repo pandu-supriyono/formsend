6e93bba76638c8edac3489df585f79f36bbc77fb
Pandu Supriyono <38923133+pandu-supriyono@users.noreply.github.com>
2023-02-19 20:47:26 +0100

    feat(config): setup ConfigModule (#1)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { validateConfig } from './config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validate: validateConfig,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { EnvironmentVariables } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService =
+    app.get<ConfigService<EnvironmentVariables, true>>(ConfigService);
+
+  await app.listen(configService.get('PORT'));
 }
 bootstrap();
