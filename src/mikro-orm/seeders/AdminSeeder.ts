2e7315ece700d595a2152cfa31ecd190bb4fa274
Pandu Supriyono <mail@supriyono.nl>
2023-02-19 21:30:36 +0100

    add AdminSeeder

---
+++ src/mikro-orm/seeders/AdminSeeder.ts
@@ -0,0 +1,32 @@
+import type { EntityManager } from '@mikro-orm/core';
+import { Seeder } from '@mikro-orm/seeder';
+import * as bcrypt from 'bcryptjs';
+import { User } from '@/modules/users/entities/user.entity';
+
+export class AdminSeeder extends Seeder {
+  async run(em: EntityManager): Promise<void> {
+    const email = process.env.ADMIN_EMAIL;
+    const password = process.env.ADMIN_PASSWORD;
+
+    if (email == null && password == null) {
+      console.log(
+        `AdminSeeder - Admin email and/or password is not set using environment variables ADMIN_EMAIL and ADMIN_PASSWORD`,
+      );
+
+      console.log('AdminSeeder - Skipping');
+
+      return;
+    }
+
+    console.log(`AdminSeeder - Seeding admin ${email}`);
+
+    const user = em.create(User, {
+      email,
+      password: bcrypt.hashSync(password),
+    });
+
+    em.persistAndFlush(user);
+
+    console.log(`AdminSeeder - Successfully seeded admin ${email}`);
+  }
+}
