f6c04132726366179bf8af7bbeb052ae65988467
Pandu Supriyono <38923133+pandu-supriyono@users.noreply.github.com>
2023-02-19 20:56:58 +0100

    feat(Logger): setup pino logger (#2)

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,20 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { Logger } from 'nestjs-pino';
 import { AppModule } from './app.module';
 import { EnvironmentVariables } from './config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
 
   const configService =
     app.get<ConfigService<EnvironmentVariables, true>>(ConfigService);
+
+  const logger = app.get<Logger>(Logger);
+
+  app.useLogger(logger);
 
   await app.listen(configService.get('PORT'));
 }
